893c9c4be6b5de070cf92c14b8aa4e674e907b66
Ignacio P <51886210+Pinto2291@users.noreply.github.com>
2023-04-03 15:29:22 -0400

    added new sections and more animations

--- app.js
+++ app.js
@@ -6,37 +6,21 @@ anime.timeline({loop: true}).add({
     targets: '.ml6 .letter',
     translateY: ["1.1em", 0],
     translateZ: 0,
-    duration: 750,
-    delay: (el, i) => 50 * i
+    duration: 2200,// 10000
+    delay: (el, i) => 100 * i
     
     }).add({
 
     targets: '.ml6',
     opacity: 0,
-    duration: 1000,
+    duration: 2000,
     easing: "easeOutExpo",
-    delay: 1000
+    delay: 1000,
 
 });
 
-// SECOND TITLE 
-
-/*
-let menu = document.querySelector('.wrapper');
-
-function toggleMenu(){
-    if(menu.classList.contains('custom')){
-        menu.classList.remove('custom')    
-    }else{
-        menu.classList.add('custom')
-        body
-    }
-}
-*/
-
 $('#menuButton').on('click', () => {
     $('.wrapper').toggleClass('custom');
-    //$('.header').nextAll().toggleClass('opacity-0');
     $('.header').nextAll().fadeToggle(650);
 })
 
@@ -45,48 +29,109 @@ $('.about-flex-art-item').on('click', (event) => {
     $(event.currentTarget).toggleClass('scale-80')
 })
 
-/*
-.on('mouseleave', (event) => {
-    $(event.currentTarget).siblings($('.about-flex-art-item')).removeClass('rotation')
-    $(event.currentTarget).removeClass('scale-80')
-})*/
 
 /* ANIMATION SECTION */
 
-let cube_1 = document.querySelector('.flex-item-1');
-let cube_2 = document.querySelector('.flex-item-2');
-let cube_3 = document.querySelector('.flex-item-3');
+let cubes = document.querySelectorAll('.flex-item');
 
-const cubeAnimation = anime.timeline({
+const flexAnimation = anime.timeline({
     loop: true,
     autoplay: false,
-    delay: 300,
+    delay: 1000,
     direction: 'alternate',
 })
 
-cubeAnimation.add({
-    targets: cube_1,
-    translateY: [0, -100],
-    duration: 1200,
+flexAnimation.add({
+    targets: cubes,
+    translateY: 0,
+    borderRadius: 10,
+    duration: 1100,
     easing: 'easeInOutQuad',
 }).add({
-    targets: cube_2,
-    width: ['10%', '50%'],
-    duration: 2000,
-    rotate: {
-        value: 360,
-        duration: 1000,
-    },
-    easing: 'easeInOutSine',
+    targets: cubes,
+    translateY: '-100%',
+    borderRadius: 20,
+    rotate: 360,
+    duration: 1100,
+    easing: 'easeInOutQuad',
 }).add({
-    targets: cube_3,
-    translateY: [0, -100],
-    duration: 1200,
-    easing: 'easeInOutQuad'
+    targets: cubes,
+    translateY: '150%',
+    borderRadius: 10,
+    rotate: -360,
+    duration: 1100,
+    easing: 'easeInOutQuad',
 })
 
+document.querySelector('.animation-play').onclick = flexAnimation.play;
+document.querySelector('.animation-pause').onclick = flexAnimation.pause;
 
 
-document.querySelector('.animation-play').onclick = cubeAnimation.play;
+let cubes2 = document.querySelectorAll('.grid-item');
 
-document.querySelector('.animation-pause').onclick = cubeAnimation.pause;
+const gridAnimation = anime.timeline({
+    loop: true,
+    autoplay: false,
+    delay: 500,
+    direction: 'alternate',
+})
+
+gridAnimation.add({
+    targets: cubes2,
+    rotate: 360,
+    duration: 1500,
+    borderRadius: 10,
+    easing: 'easeInOutQuad',
+}).add({
+    targets: cubes2,
+    rotate: -360,
+    duration: 2000,
+    borderRadius: 20,
+    easing: 'easeInOutQuad',
+})
+
+document.querySelector('.animation-play-2').onclick = gridAnimation.play;
+document.querySelector('.animation-pause-2').onclick = gridAnimation.pause;
+
+let code_animation = document.querySelector('.sec-3-anime-1');
+
+let code_animation_1 = anime({
+    targets: code_animation,
+    width: ['50%', '25%'],
+    borderRadius: '100%',
+    duration: 1500,
+    easing: 'easeInOutQuad',
+    loop: true,
+    direction: 'alternate',
+})
+
+let containerSquares = document.querySelector('.sec-3-anime-2')
+
+let ny = 10;
+let nx = 5;
+
+for (let i = 0; i < ny*nx; i++){
+    let square = document.createElement('div');
+    square.setAttribute('class', 'square');
+    containerSquares.appendChild(square)
+}
+
+
+
+anime({
+    targets: '.square',
+    //scale: ['100%', '50%'],
+    scale: [
+        {value: .1, easing: 'easeOutSine', duration: 600},
+        {value: 1, easing: 'easeInOutQuad', duration: 1200}
+      ],
+    opacity: [
+        {value: 0.3, easing: 'easeOutSine', duration: 300},
+        {value: 0.5, easing: 'easeInOutQuad', duration: 500},
+        {value: 1, easing: 'easeInOutQuad', duration: 500},
+    ],
+    delay: anime.stagger(200, {grid: [nx, ny], axis: 'x'}),
+    loop: true,
+    autoplay: true,
+    direction: 'alternate',
+});
